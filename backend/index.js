9d14c276434855d780a83aaf0e3d2a0f7f6666d3
Thanu Poptiphueng <tpcrasite@gmail.com>
2023-05-11 09:38:25 +0700

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const randomNumber = Math.random() * 10000;
+
+app.get("/*", (req, res) => {
+  res.send(`Random Number: ${randomNumber} Path: ${req.url}`);
+});
+
+app.listen(port, () => {
+  console.log(`Running server on port: ${port}`);
+});
